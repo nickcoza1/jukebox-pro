adfb9c2b984607b7916e62ad6075329265f68634
nickcoza1 <nickcoza1@gmail.com>
2025-11-16 19:59:39 -0800

    Update schema for Jukebox pro

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,25 @@
 DROP TABLE IF EXISTS playlists_tracks;
 DROP TABLE IF EXISTS playlists;
 DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text NOT NULL UNIQUE,
+  password text NOT NULL
+);
 
 CREATE TABLE tracks (
   id serial PRIMARY KEY,
   name text NOT NULL,
   duration_ms integer NOT NULL
- );
+);
 
 CREATE TABLE playlists (
   id serial PRIMARY KEY,
   name text NOT NULL,
-  description text NOT NULL
+  description text NOT NULL,
+  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE playlists_tracks (
